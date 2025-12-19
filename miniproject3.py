2eb1db840518234477d788ac46ee059b4ca87973
AyushGhole <ayushghole@gmail.com>
2025-12-19 19:03:17 +0530

    Building the 3rd mini project

--- miniproject3.py
+++ miniproject3.py
@@ -4,13 +4,14 @@ playing = input("Do you want to play? ")
 
 points = 0
 
-
+# Check whether user wants to play or not  
 if(playing.lower() == "yes"):
   print("----Start Playing----")
 else:
    print("----Game Ended----")
    quit()
 
+# 1st question
 question1 = input("What does CPU stands for ? ")
 if(question1.lower() == "central processing unit"):
    print("Correct Answer")
@@ -20,6 +21,7 @@ else:
    print("Oops!! Incorrect answer")
    points -= 5
 
+# 2nd question
 question2 = input("What does GPU stands for ? ")
 if(question2.lower() == "graphical processing unit"):
    print("Correct Answer")
@@ -28,6 +30,7 @@ else:
    print("Oops!! Incorrect answer")
    points -= 5
 
+# 3rd question
 question3 = input("What does RAM stands for ? ")
 if(question3.lower() == "random access memory" ):
    print("Correct Answer")
@@ -38,5 +41,6 @@ else:
 
 
 
+# Final score and end declaration
 print("You Scored : " , points)
 print("----Game Ended----")
